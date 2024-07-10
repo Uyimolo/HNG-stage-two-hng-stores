df2503fdca3c603b3c9f6febed5d6742e487b9e1
Urhoghide Justin Uyioghosa <uyijustindavid@gmail.com>
2024-07-10 12:42:18 +0100

    fixed responsiveness at 1800px

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard";
 
 const ProductList = () => {
   return (
-    <div className="grid grid-cols-2 gap-x-[3%] gap-y-6 md:grid-cols-3 md:gap-x-[2%] lg:grid-cols-4 lg:gap-y-12">
+    <div className="grid grid-cols-2 gap-x-[3%] gap-y-6 md:grid-cols-3 md:gap-x-[2%] lg:grid-cols-4 lg:gap-y-12 2xl:grid-cols-5">
       {productList.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
